10d31c089712034543db5032938173f09391dd28
VanManh04 <giapvanmanh290404@gmail.com>
2024-04-25 23:04:39 +0700

    Collider's collision attack

--- Assets/Scripts/Player/PlayerAnimationTriggers.cs
+++ Assets/Scripts/Player/PlayerAnimationTriggers.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngineInternal;
 
 public class PlayerAnimationTriggers : MonoBehaviour
 {
@@ -9,5 +10,16 @@ public class PlayerAnimationTriggers : MonoBehaviour
     private void AnimationTrigger()
     {
         player.AnimationTrigger();
+    }
+
+    private void AttackTrigger()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(player.attackCheck.position, player.attackCheckRadius);
+
+        foreach (var hit in colliders)
+        {
+            if (hit.GetComponent<Enemy>() != null)
+                hit.GetComponent<Enemy>().Damage();
+        }
     }
 }
