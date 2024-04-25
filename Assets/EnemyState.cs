e63d61463549317a8ba9f0e004e6c7f27baca4a2
VanManh04 <giapvanmanh290404@gmail.com>
2024-04-25 19:10:09 +0700

    Enemy Move and Idle State

--- Assets/EnemyState.cs
+++ Assets/EnemyState.cs
@@ -5,16 +5,16 @@ using UnityEngine;
 public class EnemyState
 {
     protected EnemyStateMachine stateMachine;
-    protected Enemy enemy;
+    protected Enemy enemyBase;
 
     protected bool triggerCalled;
     protected string animBoolName;
 
     protected float stateTimer;
 
-    public EnemyState(Enemy _enemy, EnemyStateMachine _startMachine, string _animBoolName)
+    public EnemyState(Enemy _enemyBase, EnemyStateMachine _startMachine, string _animBoolName)
     {
-        this.enemy = _enemy;
+        this.enemyBase = _enemyBase;
         this.stateMachine = _startMachine;
         this.animBoolName = _animBoolName;
     }
@@ -27,11 +27,11 @@ public class EnemyState
     public virtual void Enter()
     {
         triggerCalled = false;
-        enemy.anim.SetBool(animBoolName,true);
+        enemyBase.anim.SetBool(animBoolName,true);
     }
 
     public virtual void Exit()
     {
-        enemy.anim.SetBool(animBoolName,false);
+        enemyBase.anim.SetBool(animBoolName,false);
     }
 }
---
+++ Assets/SkeletonMoveState.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkeletonMoveState : EnemyState
+{
+    protected Enemy_Skeleton enemy;
+    public SkeletonMoveState(Enemy _enemyBase, EnemyStateMachine _startMachine, string _animBoolName, Enemy_Skeleton _enemy) : base(_enemyBase, _startMachine, _animBoolName)
+    {
+        this.enemy = _enemy;
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        enemy.SetVelocity(enemy.moveSpeed * enemy.facingDir, enemy.rb.velocity.y);
+
+        if (enemy.IsWallDetected() || !enemy.IsGroundDetected())
+        {
+            enemy.Flip();
+            stateMachine.ChangeState(enemy.idleState);
+        }
+    }
+}
