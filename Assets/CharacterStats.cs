80eaa00be1d7dee195fb845cda34ef13cd960b9d
VanManh04 <giapvanmanh290404@gmail.com>
2024-05-30 18:56:26 +0700

    Major stats and defensive stats

--- Assets/CharacterStats.cs
+++ Assets/CharacterStats.cs
@@ -1,14 +1,20 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CharacterStats : MonoBehaviour
 {
-    public Stat strength;
-    public Stat damage;
+    [Header("Major stats")]
+    public Stat strength; //1 point increase damage by 1 and crit.power by 1% (1đ -> damage+1, chí mạng +1)
+    public Stat agility; //1 point increase evasion by 1% and crit.chance by 1%
+    public Stat intelgence;// 1 point increase magic damage by 1 and magic resistance by 3
+    public Stat vitality;//1 point increase health by 3 or 5 points
+
+
+    [Header("Defensive stats")]
     public Stat maxHealth;
+    public Stat armor;
+    public Stat evasion;
+
+    public Stat damage;
 
     [SerializeField] private int currentHealth;
 
@@ -19,7 +25,11 @@ public class CharacterStats : MonoBehaviour
 
     public virtual void DoDamage(CharacterStats _targetStats)
     {
+        if (TargetCanAvoidAttack(_targetStats))
+            return;
+
         int totalDamage = damage.GetValue() + strength.GetValue();
+        totalDamage = CheckTargetArmor(_targetStats, totalDamage);
         _targetStats.TakeDamage(totalDamage);
     }
 
@@ -37,5 +47,24 @@ public class CharacterStats : MonoBehaviour
     {
         Debug.Log("Die");
         //throw new NotImplementedException();
+    }
+
+    private int CheckTargetArmor(CharacterStats _targetStats, int totalDamage)
+    {
+        totalDamage -= _targetStats.armor.GetValue();
+        totalDamage = Mathf.Clamp(totalDamage, 0, int.MaxValue);//giới hạn health 
+        return totalDamage;
+    }
+    private bool TargetCanAvoidAttack(CharacterStats _targetStats)
+    {
+        int totalEvasion = _targetStats.evasion.GetValue() + _targetStats.agility.GetValue();
+
+        if (Random.Range(0, 100) < totalEvasion)
+        {
+            //Debug.Log("ATTACK AVOIDED");
+            return true;
+        }
+
+        return false;
     }
 }
