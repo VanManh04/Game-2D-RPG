9a80c34cd55820fe81b815820d1e8da25e8a68d2
VanManh04 <giapvanmanh290404@gmail.com>
2024-06-01 17:06:24 +0700

    Ailments effects and duration

--- Assets/CharacterStats.cs
+++ Assets/CharacterStats.cs
@@ -24,9 +24,19 @@ public class CharacterStats : MonoBehaviour
     public Stat iceDamage;
     public Stat lightingDamage;
 
-    public bool isIgnited;
-    public bool isChilled;
-    public bool isShocked;
+    public bool isIgnited;      // does damage over time
+    public bool isChilled;      // reduce armor by 20%
+    public bool isShocked;      // reduce accuracy by 20%
+
+    private float ignitedTimer;
+    private float chilledTimer;
+    private float shockedTimer;
+
+
+    private float igniteDamageCooldown = .3f;
+    private float igniteDamageTimer;
+    private int igniteDamage;
+
 
     [SerializeField] private int currentHealth;
 
@@ -34,6 +44,36 @@ public class CharacterStats : MonoBehaviour
     {
         critPower.SetDefaultValue(150);
         currentHealth = maxHealth.GetValue();
+    }
+
+    protected virtual void Update()
+    {
+        ignitedTimer -= Time.deltaTime;
+        chilledTimer -= Time.deltaTime;
+        shockedTimer -= Time.deltaTime;
+
+        igniteDamageTimer -= Time.deltaTime;
+
+        if (ignitedTimer < 0)
+            isIgnited = false;
+
+        if (chilledTimer < 0)
+            isChilled = false;
+
+        if(shockedTimer < 0)
+            isShocked = false;
+
+        if (igniteDamageTimer < 0 && isIgnited)
+        {
+            Debug.Log("Take burn damage "+igniteDamage);
+
+            currentHealth -= igniteDamage;
+
+            if (currentHealth < 0)
+                Die();
+
+            igniteDamageTimer = igniteDamageCooldown;
+        }
     }
 
     public virtual void DoDamage(CharacterStats _targetStats)
@@ -76,7 +116,7 @@ public class CharacterStats : MonoBehaviour
         bool canApplyChill = _iceDamage > _fireDamage && _iceDamage > _lightingDamage;
         bool canApplyShock = _lightingDamage > _fireDamage && _lightingDamage > _iceDamage;
 
-        while (!canApplyIgnite&&!canApplyChill&&!canApplyShock)
+        while (!canApplyIgnite && !canApplyChill && !canApplyShock)
         {
             if (Random.value < .35f && _fireDamage > 0)
             {
@@ -103,8 +143,10 @@ public class CharacterStats : MonoBehaviour
             }
         }
 
-        _targetStats.ApplyAilments(canApplyIgnite, canApplyChill, canApplyShock);
+        if (canApplyIgnite)
+            _targetStats.SetupIgniteDamage(Mathf.RoundToInt(_fireDamage * .2f));
 
+        _targetStats.ApplyAilments(canApplyIgnite, canApplyChill, canApplyShock);
     }
 
     private static int CheckTargetResistance(CharacterStats _targetStats, int totalMagicalDamage)
@@ -119,10 +161,30 @@ public class CharacterStats : MonoBehaviour
         if (isIgnited || isChilled || isShocked)
             return;
 
+        if (_ignite)
+        {
+            isIgnited = _ignite;
+            ignitedTimer = 2;
+        }
+
+        if (_chill)
+        {
+            isChilled = _chill;
+            chilledTimer = 2;
+        }
+
+        if (_shock)
+        {
+            isShocked = _shock;
+            shockedTimer = 2;
+        }
+
         isIgnited = _ignite;
         isChilled = _chill;
         isShocked = _shock;
     }
+
+    public void SetupIgniteDamage(int _damage) => igniteDamage = _damage;
 
     public virtual void TakeDamage(int _damage)
     {
@@ -142,13 +204,20 @@ public class CharacterStats : MonoBehaviour
 
     private int CheckTargetArmor(CharacterStats _targetStats, int totalDamage)
     {
-        totalDamage -= _targetStats.armor.GetValue();
+        if (_targetStats.isChilled)
+            totalDamage -= Mathf.RoundToInt(_targetStats.armor.GetValue() * .8f);
+        else
+            totalDamage -= _targetStats.armor.GetValue();
+
         totalDamage = Mathf.Clamp(totalDamage, 0, int.MaxValue);//giới hạn health 
         return totalDamage;
     }
     private bool TargetCanAvoidAttack(CharacterStats _targetStats)
     {
         int totalEvasion = _targetStats.evasion.GetValue() + _targetStats.agility.GetValue();
+
+        if (isShocked)
+            totalEvasion += 20;
 
         if (Random.Range(0, 100) < totalEvasion)
         {
