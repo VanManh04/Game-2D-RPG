3bd9990daec804af9b359702c2df1379ae711856
VanManh04 <giapvanmanh290404@gmail.com>
2024-06-10 11:22:02 +0700

    Enemy modifiers

--- Assets/Scripts/Stats/EnemyStats.cs
+++ Assets/Scripts/Stats/EnemyStats.cs
@@ -1,16 +1,53 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyStats : CharacterStats
 {
     private Enemy enemy;
 
+    [Header("Lever details")]
+    [SerializeField] private int lever = 1;
+
+    [Range(0f, 1f)]
+    [SerializeField] private float percantageModifier = .4f;
+
     protected override void Start()
     {
+        ApplyLeverModifiers();
         base.Start();
 
         enemy = GetComponent<Enemy>();
+
+    }
+
+    private void ApplyLeverModifiers()
+    {
+        Modify(strength);
+        Modify(agility);
+        Modify(intelgence);
+        Modify(vitality);
+        
+        Modify(damage);
+        Modify(critChance);
+        Modify(critPower);
+        
+        Modify(maxHealth);
+        Modify(armor);
+        Modify(evasion);
+        Modify(magicResistance);
+
+        Modify(fireDamage);
+        Modify(iceDamage);
+        Modify(lightingDamage);
+    }
+
+    private void Modify(Stat _stat)
+    {
+        for (int i = 1; i < lever; i++)
+        {
+            float modifier = _stat.GetValue() * percantageModifier;
+
+            _stat.AddModifier(Mathf.RoundToInt(modifier));
+        }
     }
 
     public override void TakeDamage(int _damage)
