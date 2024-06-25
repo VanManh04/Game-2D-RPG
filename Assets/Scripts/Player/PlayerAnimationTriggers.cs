9bac82748c7e67155bd4255880445222bd47daf9
VanManh04 <giapvanmanh290404@gmail.com>
2024-06-25 21:15:51 +0700

    Audio Manager

--- Assets/Scripts/Player/PlayerAnimationTriggers.cs
+++ Assets/Scripts/Player/PlayerAnimationTriggers.cs
@@ -14,6 +14,8 @@ public class PlayerAnimationTriggers : MonoBehaviour
 
     private void AttackTrigger()
     {
+        AudioManager.instance.PlaySFX(2);//attack sound effect
+
         Collider2D[] colliders = Physics2D.OverlapCircleAll(player.attackCheck.position, player.attackCheckRadius);
 
         foreach (var hit in colliders)
